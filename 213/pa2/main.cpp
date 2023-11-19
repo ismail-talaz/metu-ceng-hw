d86a39e3dd23130bb04e2839fc084d0c0579ff59
ismail-talaz <sml0_tlz0@hotmail.com>
2023-11-19 17:34:34 +0300

    changes

--- 213/pa2/main.cpp
+++ 213/pa2/main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main(){
 
 
-    BST<int> myBST;
+    /*BST<int> myBST;
     BST<int> meineBST2;
 
     BST<int> *result;
@@ -34,7 +34,37 @@ int main(){
 
     result=myBST.intersection(&meineBST2);
     cout<<"Result:"<<endl;
-    result->tree2vector(result->getRoot());
+    result->tree2vector(result->getRoot());*/
+
+    string key1="eray";
+    string key2="araasy";
+    string key3="beray";
+    string key4="yusuf";
+    string key5="zuyokamk";
+    string key6="sadadsa";
+    string mov1="asdsaeray";
+    string mov2="edsadasray";
+    string mov3="vvveray";
+    string mov4="cghb";
+    string mov5="sadsada";
+    string mov6="asdjasdkasdas";
+    Movie movie1(mov1,3432,331);
+    Movie movie2(mov2,342,31231231);
+    Movie movie3(mov3,32,3131);
+    Movie movie4(mov4,11,3232);
+    Movie movie5(mov5,11,3232);
+    Movie movie6(mov5,21312,211321);
+    BST<Movie> a ,b;
+    BST<Movie> *result;
+    b.insert(key6,movie6);
+    a.insert(key1,movie1);
+    a.insert(key2,movie2);
+    a.insert(key3,movie3);
+    a.insert(key4,movie4);
+    a.insert(key5,movie5);
+    result=a.merge(&b);
+
+    cout<<"era21321y";
     
 
 
