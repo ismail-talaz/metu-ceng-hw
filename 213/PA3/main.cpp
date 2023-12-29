2d5c8f218f347c4b5ec77eb65a2697bdede7b326
ISMAIL TALAZ <e2581031@newinek>
2023-12-29 16:17:54 +0300

    main changed

--- 213/PA3/main.cpp
+++ 213/PA3/main.cpp
@@ -5,9 +5,25 @@
 using namespace std;
 int main(int argc, const char* argv[])
 {
-    CENGFlight system("input.txt");
+    /*CENGFlight system("input.txt");
     vector<string> airlineNames;
-    system.FindSisterAirlines(airlineNames,"AnadoluJet","Istanbul(IST)");
+    system.FindSisterAirlines(airlineNames,"BoraJet","Istanbul(IST)");*/
+
+    MultiGraph myGraph("input.txt");
+    myGraph.RemoveVertex("Ankara(ESB)");
+    myGraph.PrintEntireGraph();
+
+    /*myGraph.InsertVertex("Bursa");
+    myGraph.InsertVertex("İstanbul");
+    myGraph.InsertVertex("İzmir");
+    myGraph.InsertVertex("Ankara");
+    myGraph.InsertVertex("Adana");
+    myGraph.AddEdge("Ankara-Bursa Otoyolu","Ankara","Bursa",60.2,70.0);
+    myGraph.AddEdge("İzmir-İstanbul Otoyolu","İzmir","İstanbul",60.2,70.0);
+    myGraph.AddEdge("Adana-Ankara Otoyolu","Adana","Ankara",60.2,70.0);
+    myGraph.AddEdge("İstanbul-Adana Otoyolu","İstanbul","Adana",60.2,70.0);
+    myGraph.AddEdge("Bursa-İzmir Otoyolu","Bursa","İzmir",60.2,70.0);*/
+
 
     
     
